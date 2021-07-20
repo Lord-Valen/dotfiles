7a517e2d574a2c08f7054cac7f277e8773caefbc
Lord_Valen <lord_valen@protonmail.com>
2021-07-20 16:44:46 -0400

    modified:   .doom.d/README.org 	modified:   .doom.d/config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -31,6 +31,11 @@
 ;; changes certain keywords to symbols, such as lamda!
 (setq global-prettify-symbols-mode t)
 
+(after! org
+
+(setq org-directory "~/documents/org/")
+(setq org-agenda-files '("~/documents/org/agenda.org"))
+
 (setq
  org-css "file:///e:/emacs/documents/org-css/css/org.css")
 (setq
@@ -63,28 +68,29 @@
 (require 'ox-extra)
 (ox-extras-activate '(ignore-headlines))
 
-(after! org
-  ;; Import ox-latex to get org-latex-classes and other funcitonality
-  ;; for exporting to LaTeX from org
-  (use-package! ox-latex
-    :init
-    ;; code here will run immediately
-    :config
-    ;; code here will run after the package is loaded
-    (setq org-latex-pdf-process
-          '("pdflatex -interaction nonstopmode -output-directory %o %f"
-            "bibtex %b"
-            "pdflatex -interaction nonstopmode -output-directory %o %f"
-            "pdflatex -interaction nonstopmode -output-directory %o %f"))
-    (setq org-latex-with-hyperref nil) ;; stop org adding hypersetup{author..} to latex export
-    ;; (setq org-latex-prefer-user-labels t)
+;; Import ox-latex to get org-latex-classes and other funcitonality
+;; for exporting to LaTeX from org
+(use-package! ox-latex
+  :init
+  ;; code here will run immediately
+  :config
+  ;; code here will run after the package is loaded
+  (setq org-latex-pdf-process
+        '("pdflatex -interaction nonstopmode -output-directory %o %f"
+          "bibtex %b"
+          "pdflatex -interaction nonstopmode -output-directory %o %f"
+          "pdflatex -interaction nonstopmode -output-directory %o %f"))
+  (setq org-latex-with-hyperref nil) ;; stop org adding hypersetup{author..} to latex export
+  ;; (setq org-latex-prefer-user-labels t)
 
-    ;; deleted unwanted file extensions after latexMK
-    (setq org-latex-logfiles-extensions
-          (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out" "toc" "nav" "snm" "vrb" "dvi" "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl" "xmpi" "run.xml" "bcf" "acn" "acr" "alg" "glg" "gls" "ist")))
+  ;; deleted unwanted file extensions after latexMK
+  (setq org-latex-logfiles-extensions
+        (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out" "toc" "nav" "snm" "vrb" "dvi" "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl" "xmpi" "run.xml" "bcf" "acn" "acr" "alg" "glg" "gls" "ist")))
 
-    (unless (boundp 'org-latex-classes)
-      (setq org-latex-classes nil))))
+  (unless (boundp 'org-latex-classes)
+    (setq org-latex-classes nil)))
+
+)
 
 (setq creds "~/.doom.d/creds.el")
 (setq nick "lord_valen")
